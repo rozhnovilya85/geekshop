3d3918e5711d9b788b4cfca53fe2c84f698ac436
rozhnovilya85 <rozhnovilya85@gmail.com>
2022-01-18 00:45:46 +0500

    commit requirements.txt

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-qx92+q=%%=z8p!cm5yfax=88cfmu%yggoy0pl3gf40b@&c($g5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -92,10 +92,18 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres',
     }
 }
 
